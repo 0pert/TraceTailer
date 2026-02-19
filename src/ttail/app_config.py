0f7466c1f7aeb482ca26097c216ef10ac368fd8c
Oliver Persson <oliverpersson_580@hotmail.com>
2026-02-19 18:43:03 +0100

    Add storage for settings  with QSettings

---
+++ src/ttail/app_config.py
@@ -0,0 +1,23 @@
+from PyQt6.QtCore import QStandardPaths, QSettings
+from pathlib import Path
+
+
+class AppConfig:
+    def __init__(self):
+        self.settings = QSettings("BytesOfIT", "TraceTailer")
+
+        config_dir = QStandardPaths.writableLocation(
+            QStandardPaths.StandardLocation.AppDataLocation
+        )
+        self.config_path = Path(config_dir) / "TraceTailer"
+        self.config_path.mkdir(parents=True, exist_ok=True)
+        print(self.config_path)
+    def save_font_settings(self, font_size, font_name):
+        self.settings.setValue("font-size", font_size)
+        self.settings.setValue("font-name", font_name)
+
+    def load_font_settings(self):
+        return [
+            self.settings.value("font-name", "Consolas"),
+            self.settings.value("font-size", 10),
+        ]
