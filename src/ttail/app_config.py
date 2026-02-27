4ee18f30c2cb6d6f7c01884281cbca9342991a66
Oliver Persson <oliverpersson_580@hotmail.com>
2026-02-27 10:10:33 +0100

    Added style settings for content window

--- src/ttail/app_config.py
+++ src/ttail/app_config.py
@@ -11,7 +11,7 @@ class AppConfig:
         )
         self.config_path = Path(config_dir) / "TraceTailer"
         self.config_path.mkdir(parents=True, exist_ok=True)
-        
+
     def save_font_settings(self, font_size, font_name):
         self.settings.setValue("font-size", font_size)
         self.settings.setValue("font-name", font_name)
@@ -20,4 +20,14 @@ class AppConfig:
         return [
             self.settings.value("font-name", "Consolas"),
             self.settings.value("font-size", 10),
+        ]
+
+    def save_style_settings(self, text_color, bg_color):
+        self.settings.setValue("text-color", text_color)
+        self.settings.setValue("bg-color", bg_color)
+
+    def load_style_settings(self):
+        return [
+            self.settings.value("text-color", "#ffffff"),
+            self.settings.value("bg-color", "#2d2d2d"),
         ]
