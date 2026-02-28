e0f3ccf823be2a66d0a99f0958f47071e337c5fb
Oliver Persson <oliverpersson_580@hotmail.com>
2026-02-28 14:44:57 +0100

    Search and selection

--- src/ttail/search_selection.py
+++ src/ttail/search_selection.py
@@ -15,7 +15,7 @@ from PyQt6.QtGui import (
 )
 import re
 
-class SearchBar(QWidget):
+class SearchAndSelect(QWidget):
     def __init__(self, parent=None):
         super().__init__(parent)
         self.parent = parent
